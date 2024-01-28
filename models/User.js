34e45f19cca20f3e92df4ebde68a858844250f89
Jasper Liu <jasperl@berkeley.edu>
2024-01-28 00:19:54 -0800

    Finish function: Register & Login Users

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const { model, Schema} = require ('mongoose');
 
 const userSchema = new Schema({
     username: String,
-    passsword: String,
+    password: String,
     email: String,
-    creatdAt: String
+    createdAt: String
 });
 
 module.exports = model('User', userSchema);
